fc387cac93c67840c3b7fc0f123cded340d09c8d
MaltenGD <olivegar@ucm.es>
2025-01-28 00:08:13 +0100

    PlayerMovement pulido
    Ahora usa Rigidbody
    Disclaimer: la velocidad está por default en 0 (se modifica como SerializeField)
    
    Extra: he metido al camara como child del jugador para ir viendo como se mueve el jugador

--- Practica 1 MAP (Gauntlet)/Assets/Scripts/PlayerMovement.cs
+++ Practica 1 MAP (Gauntlet)/Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    private float velocity; // Variar desde la interfaz de Unity
-    // Update is called once per frame
+    private float velocity = 15; // Variar desde la interfaz de Unity
+    Rigidbody2D rb;
+    void Start()
+    {
+     rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            gameObject.transform.position += Vector3.left * velocity * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            gameObject.transform.position += Vector3.right * velocity * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            gameObject.transform.position += Vector3.up * velocity * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            gameObject.transform.position += Vector3.down * velocity * Time.deltaTime;
-        }
+        Vector3 newPos = new Vector3 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"),0);
+        rb.velocity = newPos * velocity;
+
+        // Input.GetAxisRaw("Horizontal") Devuelve -1 cuando se presiona A o LeftArrow y 1 cuando se presiona D o RightArrow
+        // Más de lo mismo para Input.GetAxisRaw("Vertical") pero con W y S o UpArrow y DownArrow respectivamente
     }
+
 }
