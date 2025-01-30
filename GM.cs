d5d86a77bb5b6691b6fb790efb7ed20ca2d8de83
sampaaaaa <chengxiang250218072807@gmail.com>
2025-01-30 16:40:22 +0100

    Add files via upload
    he cambiado el this por gameobject para que se cargue el gm en vez del componente y un return

--- GM.cs
+++ GM.cs
@@ -1,19 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject player; 
-    [SerializeField] private GameObject exit;   // Referencia al exit
-    private float second = 0f; // Para contar el tiempo y quitar 1 vida por segundo
+    [SerializeField] private GameObject player;
+    private float second = 0f; //para contar el tiempo y quitar 1 vida por segundo
+    private int score;
+    public static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+    //singleton
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) //patrón singleton
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
-    private bool isLevelComplete = false; // Verificar si el nivel fue completado
 
     // Start is called before the first frame update
     void Start()
     {
-      
+        score = 0;
     }
 
     void StartGame()
@@ -21,42 +37,27 @@ public class GameManager : MonoBehaviour
         second = 0f;
     }
 
-    // Quita 7 de vida si el player colisiona con el enemigo o avanza sin haber matado a un enemigo
+    //Quita 7 de vida si el player colisiona con el enemigo o avanza sin haber matado a un enemigo. Para comprobar esto necesitamos los enemigos
     void EnemyDamage()
     {
         player.GetComponent<Health>().Harm(7);
     }
 
-    // M�todo para cuando el jugador alcance el Exit
-    public void Exit()
+    public void TreasureCollected()
     {
-        if (!isLevelComplete)
-        {
-            isLevelComplete = true; 
-            Debug.Log("Nivel completado"); 
-            Invoke("LoadNextLevel", 2f); // Espera 2 segundos antes de cargar el siguiente nivel
-        }
-    }
-
-    
-    void LoadNextLevel()
-    {
-        Debug.Log("Cargando el siguiente nivel...");
-       
+        score += 100;
+        Debug.Log("score +100");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isLevelComplete) // Solo quitar vida si el nivel no ha terminado
-        {
-            second += Time.deltaTime;
+        second += Time.deltaTime;
 
-            if (second >= 1) // Si pasa un segundo, resta uno de vida
-            {
-                player.GetComponent<Health>().Harm(1);
-                second = 0f;
-            }
+        if (second >= 1) //si pasa un segundo, resta uno de vida. Pone >= por si se exceden los decimales al sumar por frame
+        {
+            player.GetComponent<Health>().Harm(1);
+            second = 0f;
         }
     }
 }
