979252f98acf0114ac707d93a6306d0b3c7ed695
Pablo Abellán Poza <pablabel@ucm.es>
2025-02-03 10:03:16 +0100

    exitanimation

--- Practica 1 MAP (Gauntlet)/Assets/Scripts/Exit.cs
+++ Practica 1 MAP (Gauntlet)/Assets/Scripts/Exit.cs
@@ -5,13 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
+    [SerializeField] private GameObject playerAnim;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = playerAnim.GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision) // Trigger evita que el jugador se choque con el interactuable
     {
         if (collision.gameObject.GetComponent<PlayerMovement>() != null)
         {
-            SceneManager.LoadScene("Menu");
+            StartCoroutine(ExitDelay());
 
         }
+
+    }
+
+    private IEnumerator ExitDelay()
+    {
+        animator.SetTrigger("Exit");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Menu");
+
     }
 
 }
