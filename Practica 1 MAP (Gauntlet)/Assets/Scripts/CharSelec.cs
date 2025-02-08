d857f73b9560571b13b54edb6d9c1dae1bd80003
MaltenGD <olivegar@ucm.es>
2025-02-08 22:28:19 +0100

    perdon no tuve tiempo para trabajar ahora me pongo
    ahora este mini push para evitar conflictos

--- Practica 1 MAP (Gauntlet)/Assets/Scripts/CharSelec.cs
+++ Practica 1 MAP (Gauntlet)/Assets/Scripts/CharSelec.cs
@@ -11,22 +11,19 @@ using UnityEngine.UI;
 public class CharSelec : MonoBehaviour
 {
     System.Random random = new System.Random(); // maldita ambigüedad que dolor de cabeza me ha dado
+    GameObject selection;
 
     void Start()
     {
+        selection = GameObject.FindGameObjectWithTag("CharSelection");
         EventSystem.current.SetSelectedGameObject(GetComponentsInChildren<Button>()[random.Next(0, 1)].gameObject); //mi mejor creación hasta la fecha
+        DontDestroyOnLoad(selection);
     }
 
-    public void ValkyrieSelection()
+    public void StartGameWithSelection(string character)
     {
+        selection.name = character;
         SceneManager.LoadScene(1);
-        GameManager.Instance.CharSelection("valkyrie");
-
-    }
-    public void  ElfSelection()
-    {
-        SceneManager.LoadScene(1);
-        GameManager.Instance.CharSelection("valkyrie");
 
     }
 
---
+++ Practica 1 MAP (Gauntlet)/Assets/selector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class selector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
